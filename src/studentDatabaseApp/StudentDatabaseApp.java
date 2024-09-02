46cdeb6a3fd06c87f531faced69bb002a1f86d1b
AaronNevalinz <aaronug421@gmail.com>
2024-09-02 14:53:22 +0300

    Done

--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -1,14 +1,27 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 //		ask how many users we wanna add
-		Student std1 = new Student();
-		std1.enroll();
-		std1.payTuition();
-		std1.showStatus();
+		System.out.print("Enter number of students to enroll");
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+
 //		create n number of students
+		for( int n = 0; n < numOfStudents; n++){
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		System.out.println("THESE ARE THE STUDENTS WHO HAVE SO FAR ENROLLED:\n");
+		for( int n = 0; n < numOfStudents; n++){
+			students[n].showStatus();
+		}
 
 	}
 
